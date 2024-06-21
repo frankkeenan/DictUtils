375e5095c2320377408f21b681a36300f2052206
Frank Keenan <frank.keenan@oup.com>
2024-06-21 10:38:34 +0100

    Added mechanism to retain existing annotations

--- add_annotation.pl
+++ add_annotation.pl
@@ -4,6 +4,12 @@ use autodie qw(:all);
 use open qw(:std :utf8);
 use utf8;
 use strict;
+#
+# Existing annotations are lost when annotations are imported as PIs - annotations need to be exported
+# This converts them to tags
+# The user details and date need to be copied to the content attribute or they are lost
+#
+
 our ($LOG, $LOAD, $opt_t, $opt_f, $opt_u, $opt_D, $opt_I, $opt_O, $PDIR, %W, %USED, %F, %INFO, %CHANGE, %TAGS, $p, %ALL);
 #$PDIR = ".";
 $PDIR = "/usr/local/bin/";
@@ -61,6 +67,7 @@ sub main
 	if ($opt_I){printf(bugin_fp "%s\n", $_);}	
 	# s|<!--.*?-->||gio;
 	#	next line if (m|<entry[^>]*sup=\"y|io);
+	s|<\?dps_comment(.*?)\?>|<DPS_COMMENT \1></DPS_COMMENT>|gi;
 	my $cp = $_;
 	unless (m|<e|){
 	    printf("%s\n\n", $_);
@@ -68,8 +75,9 @@ sub main
 	    next line;
 	}	
 	my $e_eid = restructure::get_tag_attval($_, "e", "e:id");
-	next line unless ($W{$e_eid});
-	$_ = &update_defs($_);
+#	next line unless ($W{$e_eid});
+#	$_ = &update_defs($_);
+	$_ = &restore_annotations($_);
 	printf("%s\n\n", $_);
 	printf(log_fp "%s\n\n", $cp); 
 	if ($opt_O){printf(bugout_fp "%s\n", $_);}
@@ -80,6 +88,44 @@ sub main
     }
     &close_debug_files;
 }
+
+##
+
+sub restore_annotations
+{
+    my($e) = @_;
+    my($res);	
+    $e =~ s|(<DPS_COMMENT.*?</DPS_COMMENT>)|&split;&fk;$1&split;|gi;
+    my @BITS = split(/&split;/, $e);
+    my $res = "";
+    foreach my $bit (@BITS){
+	if ($bit =~ s|&fk;||gi){
+	    # <?dps_comment user="ruberyj" date="2007-05-29 15:26:59" content="grammar note link" topic="updated_JR_GN"?>
+	    my $user = restructure::get_tag_attval($bit, "DPS_COMMENT", "user");
+	    my $content = restructure::get_tag_attval($bit, "DPS_COMMENT", "content");
+	    my $date = restructure::get_tag_attval($bit, "DPS_COMMENT", "date");
+	    unless ($user =~ m|frank.keenan|)
+	    {
+		$user = sprintf("user:%s", $user); 
+		$date =~ s| +.*$||;
+		$date = sprintf("date:%s", $date); 
+		if ($content =~ m|^ *$|)
+		{
+		    $content = sprintf("%s %s", $content, $user, $date); 
+		} else {
+		    $content = sprintf("%s; %s %s", $content, $user, $date); 
+		}
+		$bit = restructure::set_tag_attval($bit, "DPS_COMMENT", "content", $content); 
+	    }
+	}
+	$res .= $bit;
+    }    
+    $res =~ s|<DPS_COMMENT|<\?dps_comment|g;
+    $res =~ s|></DPS_COMMENT>|\?>|g;
+    return $res;
+}
+
+##
 
 sub update_defs
 {
